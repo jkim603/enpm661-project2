1bfda4f5ebbc742d60fe0103ac4990204ae1a632
jkim603 <113157667+jkim603@users.noreply.github.com>
2023-03-11 21:14:21 -0700

    Draft 7
    Added final path backtracking and visualization of overall solution. Also added a readme file.

---
+++ README.md
@@ -0,0 +1,12 @@
+README file for ENPM661 Project 2 for Julia Kim
+
+Code file name: dijkstra_julia_kim.py
+Dependencies: numpy, cv2, time
+Instructions:
+    1. Run the python script called "dijkstra_julia_kim.py"
+    2. Input the start state when prompted, as two integers separated by a comma. Then hit enter. Example: 10,10
+    3. Input the goal state when prompted, as two integers separated by a comma. Then hit enter. Example: 200,10
+    4. The script may run for up to 30 minutes, depending how far apart the inputs are.
+    5. When finished, the script will output a message indicating that it is complete and showing the final state it calculated. It will also show the time it took to run, in seconds.
+    6. The video file will be saved in the folder the script is running out of, with the name "proj2_video_julia_kim.mp4"
+    7. Open the folder and play the video to see the visualization of the search algorithm and final path.
--- dijkstra_julia_kim.py
+++ dijkstra_julia_kim.py
@@ -69,6 +69,16 @@ def move_up_left(i, j):
     cost = 1.4
     return new_node, cost
 
+def generate_path(node_list):
+    node_states = [item[1] for item in node_list]
+    state, parent = node_list[-1][1], node_list[-1][2]
+    path_list = [state]
+    while not parent==None:
+        ind = node_states.index(parent)
+        state, parent = node_list[ind][1], node_list[ind][2]
+        path_list.insert(0,state)
+    return path_list
+
 # define map space with 5mm clearance; 0=free space, 1=obstacle space
 map = np.zeros((250,600))
 for i in range(250):
@@ -99,47 +109,69 @@ for i in range(250):
     i = -i+249
     for j in range(600):
         # print('j',j)
-        j = -j+599
+        # j = -j+599
         if i<=100 and 100<=j<=150:
-            map_show[i,j]=np.array([255,0,125]) #bottom rectangle obstacle
+            map_show[i,j]=np.array([255,0,155]) #bottom rectangle obstacle
         elif 100<i<=105 and 100<=j<=150:
-            map_show[i,j]=np.array([255,0,200]) #bottom rectangle margin
+            map_show[i,j]=np.array([100,0,255]) #bottom rectangle margin
         elif i<=105 and (95<=j<100 or 150<j<=155):
-            map_show[i,j]=np.array([255,0,200]) #bottom rectangle margin
+            map_show[i,j]=np.array([100,0,255]) #bottom rectangle margin
         elif i>=150 and 100<=j<=150:
-            map_show[i,j]=np.array([255,0,125]) #top rectangle obstacle
+            map_show[i,j]=np.array([255,0,155]) #top rectangle obstacle
         elif 150>i>=145 and 100<=j<=150:
-            map_show[i,j]=np.array([255,0,200]) #top rectangle margin
+            map_show[i,j]=np.array([100,0,255]) #top rectangle margin
         elif i>=145 and (95<=j<100 or 150<j<=155):
-            map_show[i,j]=np.array([255,0,200]) #top rectangle margin
+            map_show[i,j]=np.array([100,0,255]) #top rectangle margin
         elif j>=460 and i>=2*j-895 and i<=-2*j+1145: 
-            map_show[i,j]=np.array([255,0,125]) #triangle obstacle
+            map_show[i,j]=np.array([255,0,155]) #triangle obstacle
         elif 460>j>=455 and 2*j-895<=i<=-2*j+1145 and 20<=i<=230:
-            map_show[i,j]=np.array([255,0,200]) #triangle margin
+            map_show[i,j]=np.array([100,0,255]) #triangle margin
         elif 510>=j>=455 and ((2*j-895>i>=2*j-906 and i>=20) or (-2*j+1145<i<=-2*j+1156 and i<=230)):
-            map_show[i,j]=np.array([255,0,200]) #triangle margin
+            map_show[i,j]=np.array([100,0,255]) #triangle margin
         elif 515>=j>510 and 2*j-906<=i<=-2*j+1156:
-            map_show[i,j]=np.array([255,0,200]) #triangle margin
+            map_show[i,j]=np.array([100,0,255]) #triangle margin
         elif 235<=j<=365 and i<=0.5774*j+26.7949 and i<=-0.5774*j+373.2051 and i>=-0.5774*j+223.2051 and i>=0.5774*j-123.2051:
-            map_show[i,j]=np.array([255,0,125]) #hexagon obstacle
+            map_show[i,j]=np.array([255,0,155]) #hexagon obstacle
         elif 230<=j<235 and 0.5774*j-48.2051<=i<=-0.5774*j+298.2051:
-            map_show[i,j]=np.array([255,0,200]) #hexagon margin
+            map_show[i,j]=np.array([100,0,255]) #hexagon margin
         elif 365<j<=370 and 0.5774*j-48.2051>=i>=-0.5774*j+298.2051:
-            map_show[i,j]=np.array([255,0,200]) #hexagon margin
+            map_show[i,j]=np.array([100,0,255]) #hexagon margin
         elif 230<=j<=300 and (0.5774*j+26.7949<i<=0.5774*j+32.7949 or -0.5774*j+223.2051>i>=-0.5774*j+217.2051):
-            map_show[i,j]=np.array([255,0,200]) #hexagon margin
+            map_show[i,j]=np.array([100,0,255]) #hexagon margin
         elif 300<=j<=370 and (-0.5774*j+373.2051<i<=-0.5774*j+379.2051 or 0.5774*j-123.2051>i>=0.5774*j-129.2051):
-            map_show[i,j]=np.array([255,0,200]) #hexagon margin
+            map_show[i,j]=np.array([100,0,255]) #hexagon margin
         elif 0<=j<=4 or 595<=j<=599: 
-            map_show[i,j]=np.array([255,0,200]) #vertical wall margin
+            map_show[i,j]=np.array([100,0,255]) #vertical wall margin
         elif 0<=i<=4 or 245<=i<=249: 
-            map_show[i,j]=np.array([255,0,200]) #horizonal wall margin
+            map_show[i,j]=np.array([100,0,255]) #horizonal wall margin
 
-start_input = input("Start State:")
-goal_input = input("Goal State:")
+# request inputs and check that they are within map bounds and not in an obstacle
+while 1:
+    try:
+        start_input = input("Start State: x,y:")
+        node_state_i = input2node(start_input)
+        # if node_state_i[0]<0 or node_state_i[0]>599 or node_state_i[1]<0 or node_state_i[1]>249:
+        #     print('Start State outside of acceptable input range. X input range: 1 to 600. Y input range: 1 to 250. Try again...')
+        if map[node_state_i[1],node_state_i[0]]==1:
+            print('Start State inside an obstacle. Try again...')
+        else:
+            break
+    except:
+        print('Input must be two integers separated by a comma (ex: 10,10). Acceptable range for first value: 1 to 600. Acceptable range for second value: 1 to 250. Try again...')
 
-node_state_i = input2node(start_input)
-goal_state = input2node(goal_input)
+while 1:
+    try:
+        goal_input = input("Goal State: x,y:")
+        goal_state = input2node(goal_input)
+        # if goal_state[0]<0 or goal_state[0]>599 or goal_state[1]<0 or goal_state[1]>249:
+        #     print('Goal State outside of acceptable input range. X input range: 1 to 600. Y input range: 1 to 250. Try again...')
+        if map[goal_state[1],goal_state[0]]==1:
+            print('Goal State inside an obstacle. Try again...')
+        else:
+            print('Inputs accepted! Calculating...')
+            break
+    except:
+        print('Input must be two integers separated by a comma (ex: 10,10). Acceptable range for first value: 1 to 600. Acceptable range for second value: 1 to 250. Try again...')
 
 node_parent_i = None
 node_cost_i = 0
@@ -150,12 +182,11 @@ nodes_closed = []
 
 nodes_open.sort(key = lambda x: x[0])
 current_node = nodes_open.pop(0)
-current_cost = current_node[0]
-current_state = current_node[1]
+current_cost, current_state = current_node[0], current_node[1]
 
 # pop a node, generate child nodes, compare and update lists
 while not current_state==goal_state:
-    print(current_state)
+    # print(current_state)
     node_parent_i, x_coord, y_coord = current_state, current_state[0], current_state[1]
     nodes_closed.append(current_node)
 
@@ -312,14 +343,45 @@ while not current_state==goal_state:
                 break
 
     nodes_open.sort(key = lambda x: x[0])
-    current_node, current_cost, current_state = nodes_open.pop(0), current_node[0], current_node[1]
+    current_node = nodes_open.pop(0)
+    current_cost, current_state = current_node[0], current_node[1]
 
+nodes_closed.append(current_node)
+print("Complete! Confirm final state:",'('+str(current_state[0]+1)+', '+str(current_state[1]+1)+')')
 
+path = generate_path(nodes_closed)
+# print(path)
 
+print('check 1')
+# visualize map grid search and final path
+img_array = [map_show]
+print('check 1a')
+for node in nodes_closed:
+    print('check 1b')
+    x_coord, y_coord = node[1][0], 249-node[1][1]
+    print('check 1c')
+    map_show[y_coord, x_coord] = np.array([220,255,0]) #update color of pixels as they are explored
+    print('check 1d')
+    img_array.append(map_show.copy())
+    print('check 1e')
+print('check 2')
+for state in path:
+    x_coord, y_coord = state[0], 249-state[1]
+    map_show[y_coord, x_coord] = np.array([200,0,0]) #update color of optimal path as it is traveled
+    img_array.append(map_show.copy())
+
+print('check 3')
+# write to video file
+size = (600,250)
+vid = cv.VideoWriter('proj2_video_julia_kim.mp4', cv.VideoWriter_fourcc(*'mp4v'), 1000, size)
+print('check 4')
+for k in range(len(img_array)):
+    img = img_array[k]
+    vid.write(img)
+print('check 5')
+vid.release()
+cv.destroyAllWindows()
+
+print('check 6')
 end_time = time.time()
 print('Total time (s):', end_time-start_time)
-
-# cv.imshow('Binary Map',map)
-# cv.imshow('Colorful Map',map_show)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
